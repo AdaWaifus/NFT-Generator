6bc6d07d7cdd2749225c470f6de19e20e7f60ff4
Sven Budak <sven@budak.info>
2022-07-17 23:27:16 +0200

    CSS Fix

--- frontend/src/app/shared/mat.module.ts
+++ frontend/src/app/shared/mat.module.ts
@@ -3,9 +3,17 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatDialogModule} from "@angular/material/dialog";
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
-const matModules = [MatDialogModule, MatExpansionModule, MatCheckboxModule, MatButtonModule, MatIconModule];
+const matModules = [
+  MatDialogModule,
+  MatExpansionModule,
+  MatCheckboxModule,
+  MatButtonModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+];
 
 @NgModule({
   imports: [matModules],
